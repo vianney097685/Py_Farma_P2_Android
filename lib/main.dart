d0e56222611842fdb46a4dcc2f8f5c0c6ad1052c
FlutLab.io <>
2021-05-12 22:22:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MelendezApp());
+void main() => runApp(MyApp());
 
-class MelendezApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Mi Empresa',
-        theme: ThemeData(primarySwatch: Colors.red),
-        //ruta de ventanas
-        routes: <String, WidgetBuilder>{
-          "/inicio": (BuildContext context) => Inicio(),
-          "/empresa": (BuildContext context) => Empresa(),
-          "/productos": (BuildContext context) => Productos(),
-          "/contacto": (BuildContext context) => Contacto(),
-        }, //Fin de rutas
-        home: Inicio()); //Fin de material
-  } //Fin widget
-} //Fin MelendezApp
+      debugShowCheckedModeBanner: false,
+      title: 'My Relax App',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      //ruta de ventanas
+      routes: <String, WidgetBuilder>{
+        "/inicio": (BuildContext) => Inicio(),
+        "/empresa": (BuildContext) => Empresa(),
+        "/productos": (BuildContext) => Productos(),
+        "/contacto": (BuildContext) => Contacto(),
+      }, //fin de rutas
+      home: Inicio(),
+    ); //Fin de la app material
+  } //Fin del widget
+} //Fin de clase relax app
 
 class Empresa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: new Text("Empresa"),
-      ),
+        title: new Text('Nuestra Empresa'),
+      ), // Fin de appbar
       body: Center(
-        child: Text("Seccion Empresa"),
-      ),
+        child: Text('Sección\nEmpresa'),
+      ), //Fin de body center
     ); //Fin de Scaffold
-  } //Fin widget empresa
-} //Fin clase Empresa
+  } //Fin de widget
+} //Fin de clase Empresa
 
 class Productos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: new Text("Productos"),
-      ),
+        title: new Text('Nuestros Productos'),
+      ), // Fin de appbar
       body: Center(
-        child: Text("Seccion Productos"),
-      ),
+        child: Text('Sección\nProductos'),
+      ), //Fin de body center
     ); //Fin de Scaffold
-  } //Fin widget productos
-} //Fin clase Productos
+  } //Fin de widget
+} //Fin de clase Productos
 
 class Contacto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: new Text("Contacto"),
-      ),
+        title: new Text('Contáctanos'),
+      ), // Fin de appbar
       body: Center(
-        child: Text("Seccion Contacto"),
-      ),
+        child: Text('Sección\nContacto'),
+      ), //Fin de body center
     ); //Fin de Scaffold
-  } //Fin widget contacto
-} //Fin clase Contacto
+  } //Fin de widget
+} //Fin de clase Contacto
 
 class Inicio extends StatelessWidget {
   @override
@@ -68,8 +71,15 @@ class Inicio extends StatelessWidget {
     return new Scaffold(
       body: Container(
         padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10),
-        decoration: BoxDecoration(color: Colors.grey, image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/jaquelineME/mis_imagenes/main/MicrosoftTeams-image.png"), alignment: Alignment.topCenter)), //Fin de la caja
-      ), //Fin de Container
-    ); //Fin de Scaffold
-  } //Fin del widget inicio
-} //Fin de Inicio
+        decoration: BoxDecoration(
+          color: Colors.teal[100],
+          image: DecorationImage(
+            image: NetworkImage('https://raw.githubusercontent.com/vianney097685/mis_imagenes/main/vacunas-consejos-peru.jpg'),
+            alignment: Alignment.topCenter,
+          ),
+          borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+        ), //Fin de caja 
+      ), //Fin del contenedor
+    ); //Fin de scaffold
+  } //Fin de widget
+} //Fin de clase Inicio
