09041a87e34545cbae4abc4089fbcf4b49c1b1d6
FlutLab.io <>
2021-05-13 00:38:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -102,7 +102,7 @@ class Inicio extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.all(10),
                   child: RaisedButton(
-                    color: Colors.redAccent,
+                    color: Colors.blueAccent,
                     shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                     onPressed: () {
                       Navigator.pushNamed(context, "/empresa");
