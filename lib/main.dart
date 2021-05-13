08dba5f935347c48411bbbab148d2c9adfeb8b14
FlutLab.io <>
2021-05-13 00:41:22 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class Inicio extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.all(10),
                   child: RaisedButton(
-                    color: Colors.redAccent,
+                    color: Colors.blue[200],
                     shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                     onPressed: () {
                       Navigator.pushNamed(context, "/inicio");
@@ -102,7 +102,7 @@ class Inicio extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.all(10),
                   child: RaisedButton(
-                    color: Colors.blueAccent,
+                    color: Colors.blue[200],
                     shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                     onPressed: () {
                       Navigator.pushNamed(context, "/empresa");
@@ -128,7 +128,7 @@ class Inicio extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: RaisedButton(
-                      color: Colors.redAccent,
+                      color: Colors.blueAccent,
                       shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                       onPressed: () {
                         Navigator.pushNamed(context, "/productos");
@@ -149,7 +149,7 @@ class Inicio extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: RaisedButton(
-                      color: Colors.redAccent,
+                      color: Colors.blueAccent,
                       shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                       onPressed: () {
                         Navigator.pushNamed(context, "/contacto");
