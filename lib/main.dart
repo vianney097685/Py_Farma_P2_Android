707c6c6481c1288416518ccaaa18e90fe06c7d23
FlutLab.io <>
2021-05-13 00:21:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyFarmaciaApp());
+void main() => runApp(MelendezApp());
 
-class MyFarmaciaApp extends StatelessWidget {
+class MelendezApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Mi Farmacia",
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.lightBlue,
-        ), //Fin de theme
-
-        //Ruta de ventanas
+        title: 'Mi Empresa',
+        theme: ThemeData(primarySwatch: Colors.red),
+        //ruta de ventanas
         routes: <String, WidgetBuilder>{
           "/inicio": (BuildContext context) => Inicio(),
           "/empresa": (BuildContext context) => Empresa(),
           "/productos": (BuildContext context) => Productos(),
           "/contacto": (BuildContext context) => Contacto(),
         }, //Fin de rutas
-
-        home: Inicio()); //Fin de Material
-  } //Fin de widget
-} //Fin de Farmacia
+        home: Inicio()); //Fin de material
+  } //Fin widget
+} //Fin MelendezApp
 
 class Empresa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Empresa'),
-      ), //Fin de appBar
+        title: new Text("Empresa"),
+      ),
       body: Center(
-        child: Text(
-          'Seccion de empresa',
-        ), //Fin de child: Text
-      ), //Fin de body: Center
+        child: Text("Seccion Empresa"),
+      ),
     ); //Fin de Scaffold
-  } //Fin del widget Empresa
-} //Fin de empresa
+  } //Fin widget empresa
+} //Fin clase Empresa
 
 class Productos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Productos'),
-      ), //Fin de appBar
+        title: new Text("Productos"),
+      ),
       body: Center(
-        child: Text(
-          'Seccion de productos',
-        ), //Fin de child: Text
-      ), //Fin de body: Center
+        child: Text("Seccion Productos"),
+      ),
     ); //Fin de Scaffold
-  } //Fin del widget productos
-} //Fin de productos
+  } //Fin widget productos
+} //Fin clase Productos
 
 class Contacto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Contacto'),
-      ), //Fin de appBar
+        title: new Text("Contacto"),
+      ),
       body: Center(
-        child: Text(
-          'Seccion de contacto',
-        ),
-      ), //Fin de body: Center
+        child: Text("Seccion Contacto"),
+      ),
     ); //Fin de Scaffold
-  } //Fin del widget contacto
-} //Fin de contacto
+  } //Fin widget contacto
+} //Fin clase Contacto
 
 class Inicio extends StatelessWidget {
   @override
@@ -78,9 +68,108 @@ class Inicio extends StatelessWidget {
     return new Scaffold(
       body: Container(
         padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10),
-        decoration: BoxDecoration(color: Colors.pink, image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/vianney097685/mis_imagenes/main/vacunas-consejos-peru.jpg"), alignment: Alignment.topCenter) //decorationImage
-            ), //Fin caja
-      ), //Fin container
-    ); //Fin Scaffold
-  } //Fin widget inicio
+        decoration: BoxDecoration(color: Colors.grey, image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/jaquelineME/mis_imagenes/main/motor_carro.jpg"), alignment: Alignment.topCenter)), //Fin de la caja
+        child: Column(children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: RaisedButton(
+                    color: Colors.redAccent,
+                    shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/inicio");
+                    },
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Center(
+                        child: Text(
+                          "INICIO",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
+                        ),
+                      ), //Fin center
+                    ), //Fin sizebox
+                  ), //fin boton inicio
+                ) //Fin de padding
+              ], //Fin de widget[]
+            ), //Fin columna interna
+
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: RaisedButton(
+                    color: Colors.redAccent,
+                    shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/empresa");
+                    },
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Center(
+                        child: Text("EMPRESA", textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900)),
+                      ),
+                    ),
+                  ),
+                ) //pading
+              ], //fin widget
+            ), //Fin columna 2
+          ] //Widget[]
+              ), //Fin de fila 1
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      color: Colors.redAccent,
+                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/productos");
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Center(
+                          child: Text("PRODUCTOS", textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      color: Colors.redAccent,
+                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/contacto");
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Center(
+                          child: Text("CONTACTO", textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ] //Fin del widget
+            ), //Columna
+      ), //Fin de Container
+    ); //Fin de Scaffold
+  } //Fin del widget inicio
 } //Fin de Inicio
